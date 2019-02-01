6931fe274c2bf1350e8d41e3c93e24bd2f62aa39
Aleksander Wozniak <4wozniak@gmail.com>
2019-02-01 18:28:36 +0100

    Styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def welcome
-    @commenters = Comment.joins(:user).group(:user_id).where('comments.created_at >= ?', 7.days.ago).order('count(user_id) DESC').select('users.id', 'users.name', 'count(user_id) as comments_count').limit(10).as_json
+    @commenters = Comment.joins(:user)
+      .group(:user_id)
+      .where('comments.created_at >= ?', 7.days.ago)
+      .select('users.id', 'users.name', 'count(user_id) as comments_count')
+      .order('comments_count DESC')
+      .limit(10).as_json
   end
 end
