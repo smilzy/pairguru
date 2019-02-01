01373d12e5f7b34435b569ad3dcbf17181fad9c0
Aleksander Wozniak <4wozniak@gmail.com>
2019-02-01 10:30:20 +0100

    Create MovieExporterJob, call it from MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
 
   def export
     file_path = "tmp/movies.csv"
-    MovieExporter.new.call(current_user, file_path)
+    MovieExporterJob.new(current_user, file_path).perform_later
     redirect_to root_path, notice: "Movies exported"
   end
 end
