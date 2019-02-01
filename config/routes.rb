d94c58491cb35b5dba64265e8b05a48831e331af
Aleksander Wozniak <4wozniak@gmail.com>
2019-02-01 17:39:42 +0100

    Display movie comments, allow user to comment on movie unless he has done it already

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show] do
     member do
       get :send_info
+      post :add_comment
+      delete :delete_comment
     end
     collection do
       get :export
