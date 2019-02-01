7421657fc254bbc395ea954888a74305c33f4234
Aleksander Wozniak <4wozniak@gmail.com>
2019-02-01 17:57:09 +0100

    Display top commenters from last 7 days at welcome screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
-  def welcome; end
+  def welcome
+    @commenters = Comment.joins(:user).group(:user_id).where('comments.created_at >= ?', 7.days.ago).order('count(user_id) DESC').select('users.id', 'users.name', 'count(user_id) as comments_count').limit(10).as_json
+  end
 end
